51f93c1e8ad6b951c57dae844573db3e8d2be57e
Armen Baghdasaryan <armen.baghdasaryan91@gmail.com>
2024-04-04 17:37:24 +0400

    feat: add env file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
 
 app.use(cors());
 
